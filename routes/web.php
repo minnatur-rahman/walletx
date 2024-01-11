c506f64b0b7a13fc674f0dcb85b03eddd490fa5c
millatur <minnatur7373@gmail.com>
2024-01-12 02:48:59 +0600

    edit model and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/categories',[CategoryController::class, 'index'])->name('categories');
+
